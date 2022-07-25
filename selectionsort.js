d12079c2f5be5f2bfcbdeb0e15129c0acf7d2732
Taha Hussain <100864222+TahaHussain0911@users.noreply.github.com>
2022-07-25 15:23:45 +0500

    Update selectionsort.js

--- selectionsort.js
+++ selectionsort.js
@@ -1,13 +1,12 @@
-function selectionsort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                let temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+function selectionsort(arr){
+    for (let i = 0; i < arr.length; i++) {
+        let switching;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[i]>arr[j]) {
+                switching=j;
             }
         }
-        console.log(arr)
+        [arr[i],arr[switching]]=[arr[switching],arr[i]];
     }
     return arr;
 }
